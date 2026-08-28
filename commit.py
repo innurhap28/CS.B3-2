e3bfe40c096207c661476c11c4b9c3644990df59
innurhap28 <kmugi118@gmail.com>
2026-08-29 00:06:05 +0900

    feat: Commit 객체 정의 및 init_repository 메서드 작성

--- commit.py
+++ commit.py
@@ -1,4 +1,9 @@
 # Commit 객체 정의
 
 class Commit:
-    pass
+    def __init__(self, commit_hash, message, author, timestamp, parents):
+        self.hash = commit_hash
+        self.message = message
+        self.author = author
+        self.timestamp = timestamp
+        self.parents = parents
--- repository.py
+++ repository.py
@@ -1,1 +1,14 @@
 # 커밋 저장소, HEAD, 현재 사용자 관리
+
+class Repository:
+    def __init__(self):
+        self.commits = {}
+        self.branches = {}
+        self.head = None
+        self.current_user = None
+
+    def init_repository(self, user_name):
+        self.current_user = user_name
+
+        self.branches["main"] = None
+        self.head = "main"
