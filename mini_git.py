044f174f1b4186ea8faa8e15274980f526f81659
innurhap28 <kmugi118@gmail.com>
2026-09-05 21:05:14 +0900

    feat: Insertion Sort 구현

--- mini_git.py
+++ mini_git.py
@@ -1,1 +1,9 @@
 # CLI 명령을 실제 기능과 연결
+
+from repository import Repository
+from graph import Graph
+from sort import Sort
+
+class MiniGit:
+    def __init__(self):
+        pass
--- sort.py
+++ sort.py
@@ -1,1 +1,24 @@
 # 직접 구현한 정렬 알고리즘
+
+class Sort:
+    def insertion_sort(self, commits, key):
+        result = []
+
+        for commit in commits:
+            result.append(commit)
+
+            i = len(result) - 1
+
+            while i > 0:
+                current = result[i]
+                previous = result[i - 1]
+
+                if key(current) >= key(previous):
+                    break
+
+                result[i] = previous
+                result[i - 1] = current
+
+                i -= 1
+
+        return result
